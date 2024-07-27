500739294a698d55f67af3faaeed2786f0f106f1
hubCandy <3183476618@qq.com>
2024-07-27 15:26:04 +0800

    Update abc.java

--- src/main/java/abc.java
+++ src/main/java/abc.java
@@ -1,7 +1,7 @@
 public class abc {
     public static void main(String[] args) {
         System.out.println("master");
-        System.out.println("two");
+        System.out.println("one");
         System.out.println("pull");
         //git 改变
     }
